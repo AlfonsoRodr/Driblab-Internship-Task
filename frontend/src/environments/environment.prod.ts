84e799a301c7add71489abf4dc7c584f55e0bc20
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-12-12 12:52:01 +0100

    Fixes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	apiUrl: 'https://api.football-data.org/v4',
+	apiUrl: 'http://api.football-data.org/v4',
 	apiKey: '6fc6c54ba0ff451c8e929f4811c67948',
 };
